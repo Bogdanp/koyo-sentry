436f3b1d6a3591647eaf607d1cfd7d533296d28e
Bogdan Popa <bogdan@defn.io>
2023-10-03 10:25:39 +0300

    build: require sentry-lib 0.2.1+

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define verison "0.1")
+(define verison "0.1.1")
 (define collection "koyo")
 
 (define deps '("base"
                "koyo-lib"
-               "sentry-lib"
+               ["sentry-lib" #:version "0.2.1"]
                "web-server-lib"))
 (define build-deps '("racket-doc"
                      "scribble-lib"
