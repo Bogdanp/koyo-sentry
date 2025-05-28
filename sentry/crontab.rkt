e4e4e78716c5b7c61e9bda093dac87a4eb8484b1
Bogdan Popa <bogdan@defn.io>
2025-05-28 15:02:02 +0300

    crontab: preserve wrapped procedure names

--- sentry/crontab.rkt
+++ sentry/crontab.rkt
@@ -7,15 +7,18 @@
 (provide
  wrap-sentry/cron)
 
-(define (((wrap-sentry/cron sentry) proc) timestamp)
-  (with-timing 'sentry "wrap-sentry/cron"
-    (parameterize ([current-sentry sentry])
-      (call-with-transaction
-        #:source 'component
-        (format "crontab.~a" (object-name proc))
-        (lambda (_)
-          (with-handlers ([exn:fail?
-                           (lambda (e)
-                             (sentry-capture-exception! e)
-                             (raise e))])
-            (proc timestamp)))))))
+(define ((wrap-sentry/cron sentry) proc)
+  (procedure-rename
+   (lambda (timestamp)
+     (with-timing 'sentry "wrap-sentry/cron"
+       (parameterize ([current-sentry sentry])
+         (call-with-transaction
+           #:source 'component
+           (format "crontab.~a" (object-name proc))
+           (lambda (_)
+             (with-handlers ([exn:fail?
+                              (lambda (e)
+                                (sentry-capture-exception! e)
+                                (raise e))])
+               (proc timestamp)))))))
+   (object-name proc)))
