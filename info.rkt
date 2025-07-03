222e6ace17f235b6d80a9b8304f8cdf8b4ac51b2
Bogdan Popa <bogdan@defn.io>
2025-07-03 13:11:28 +0300

    http: improve request context

--- info.rkt
+++ info.rkt
@@ -1,13 +1,13 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.5.1")
+(define version "0.6")
 (define collection "koyo")
 (define deps
   '("base"
     ["component-lib" #:version "1.3"]
     ["koyo-lib" #:version "0.24"]
-    ["sentry-lib" #:version "0.4"]
+    ["sentry-lib" #:version "0.6"]
     "threading-lib"
     "web-server-lib"))
 (define build-deps
--- sentry/http.rkt
+++ sentry/http.rkt
@@ -2,7 +2,7 @@
 
 (require koyo/http
          koyo/profiler
-         net/uri-codec
+         koyo/url
          net/url
          racket/match
          sentry
@@ -17,33 +17,37 @@
 
 (define (((wrap-sentry sentry) hdl) req)
   (with-timing 'sentry "wrap-sentry"
-    (parameterize ([current-sentry sentry])
-      (define data (request-trace-data req))
-      (define-values (trace-id parent-id _sampled?)
-        (request-trace-ids req))
-      (call-with-transaction
-        #:data data
-        #:source 'url
-        #:trace-id trace-id
-        #:parent-id parent-id
-        #:operation 'http.server
-        (request-txn-name req)
-        (lambda (t)
-          (define res
-            (with-handlers ([can-be-ignored?
-                             (lambda (e)
-                               (log-koyo-sentry-debug "exception ~v ignored" (exn-message e))
-                               (raise e))]
-                            [exn:fail?
-                             (lambda (e)
-                               (log-koyo-sentry-debug "capturing exception ~v" (exn-message e))
-                               (sentry-capture-exception! #:request req e)
-                               (span-set! t 'http.response.status_code 500)
-                               (raise e))])
-              (hdl req)))
-          (begin0 res
-            (when (response? res)
-              (span-set! t 'http.response.status_code (response-code res)))))))))
+    (cond
+      [(capture-request? req)
+       (parameterize ([current-sentry sentry])
+         (define-values (trace-id parent-id _sampled?)
+           (request-trace-ids req))
+         (call-with-transaction
+           #:origin 'auto.http.koyo
+           #:source 'url
+           #:request (request-data req)
+           #:trace-id trace-id
+           #:parent-id parent-id
+           #:operation 'http.server
+           (request-txn-name req)
+           (lambda (t)
+             (define res
+               (with-handlers ([can-be-ignored?
+                                (lambda (e)
+                                  (log-koyo-sentry-debug "exception ~v ignored" (exn-message e))
+                                  (raise e))]
+                               [exn:fail?
+                                (lambda (e)
+                                  (log-koyo-sentry-debug "capturing exception ~v" (exn-message e))
+                                  (sentry-capture-exception! #:request req e)
+                                  (span-set! t 'http.response.status_code 500)
+                                  (raise e))])
+                 (hdl req)))
+             (when (response? res)
+               (span-set! t 'http.response.status_code (response-code res)))
+             res)))]
+      [else
+       (hdl req)])))
 
 (define (request-trace-ids req)
   (define maybe-parts
@@ -58,23 +62,30 @@
     [`(,trace-id ,parent-id ,sampled) (values trace-id parent-id (equal? sampled "1"))]
     [_ (values #f #f #t)]))
 
-(define (request-trace-data req)
-  (define uri
-    (request-uri req))
+(define (request-data req)
+  (define u (request-uri req))
   (hasheq
-   'http.method (bytes->string/utf-8 (request-method req))
-   'url.scheme (or (url-scheme uri) "http")
-   'url.path (url-path* uri)
-   'url.query (url-query* uri)))
+   'url (~url u)
+   'method (bytes->string/utf-8 (request-method req))
+   'query_string
+   (for*/list ([p (in-list (url-query u))]
+               [k (in-value (symbol->string (car p)))]
+               [v (in-value (cdr p))])
+     (list k (or v "")))))
 
 (define (request-txn-name req)
   (format "~a ~a"
           (request-method req)
           (url-path* (request-uri req))))
 
-(define (url-query* u)
-  (alist->form-urlencoded (url-query u)))
-
 (define (can-be-ignored? e)
   (or (exn:fail:servlet-manager:no-continuation? e)
       (exn:fail:servlet-manager:no-instance? e)))
+
+(define (capture-request? req)
+  (member
+   (request-method req)
+   '(#"CONNECT" #"DELETE" #"GET" #"PATCH" #"POST" #"PUT" #"TRACE")))
+
+(define (~url u)
+  (apply make-application-url (map path/param-path (url-path u))))
